f95033fd7749cbc262f30022de1a032cd9ad4d1b
Xinqian liu <crystalynliu@gmail.com>
2016-07-18 21:21:56 +0800

    init tools and files

---
+++ README.md
@@ -0,0 +1,7 @@
+# 3week3page-boilerplate 
+### how to setup local development env?
+1. npm install
+2. gulp images
+3. gulp sass
+4. gulp serve
+
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gls = require('gulp-live-server');
+var browserSync = require('browser-sync').create()
+
+gulp.task('sass', function () {
+  return gulp.src('./src/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+  return gulp.src('./src/images/**')
+    .pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./"
+  });
+
+  gulp.watch("./src/*.scss", ['sass']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch('./src/images/***');
+
+  gulp.task('default', ['serve']);
+});
